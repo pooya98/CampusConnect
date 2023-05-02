39bdbed3fee3294cb15a839f35aa7527203653e3
pooya98 <pooya98@naver.com>
2023-05-02 18:39:10 +0900

    Project 생성

---
+++ CampusConnect/CampusConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusConnectApp.swift
+//  CampusConnect
+//
+//  Created by 강승우 on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct CampusConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
