f41cc22ba2d621c5885969fb574a8baed468bd1d
Getsug <riderlua@protonmail.com>
2023-05-25 16:58:21 +0900

    email reauth modification

---
+++ CampusConnect/Settings/EmailReauthView.swift
@@ -0,0 +1,20 @@
+//
+//  EmailReauthView.swift
+//  CampusConnect
+//
+//  Created by Herbert on 2023/05/10.
+//
+
+import SwiftUI
+
+struct EmailReauthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmailReauthView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmailReauthView()
+    }
+}
