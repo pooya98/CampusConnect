9ca4af2c36a0ca1980e9e6dd1077b59122fd5231
pooya98 <pooya98@naver.com>
2023-06-18 19:48:03 +0900

    <GroupCategory>, <Group>, <Story> Model 추가

---
+++ CampusConnect/Models/GroupModel.swift
@@ -0,0 +1,8 @@
+//
+//  GroupModel.swift
+//  CampusConnect
+//
+//  Created by 강승우 on 2023/06/17.
+//
+
+import Foundation
---
+++ CampusConnect/Models/StoryModel.swift
@@ -0,0 +1,14 @@
+//
+//  Story2.swift
+//  CampusConnect
+//
+//  Created by 이헌규 on 2023/06/08.
+//
+
+import Foundation
+
+struct Story: Identifiable {
+    let id = UUID()
+    let imageName: String
+    let username: String
+}
