9817c5a1ecfddef9622e14d5f27c0f3c38e7da98
pooya98 <pooya98@naver.com>
2023-06-18 19:49:42 +0900

    <SplashView>, <OnboardingView> view 추가

--- CampusConnect/PreView/OnboardingView.swift
+++ CampusConnect/PreView/OnboardingView.swift
@@ -5,89 +5,62 @@
 //  Created by 강승우 on 2023/05/02.
 //
 
-/*
-import SwiftUI
-
-// OnboardingView 정의
-struct OnboardingView: View {
-    var body: some View {
-        // 수직으로 컨트롤을 정렬
-        VStack {
-            // Spacer() <- 가능한 많은 공간을 차지하여 이후의 컨트롤을 아래쪽으로 밀어냄
-            // Spacer()에 minLength를 설정하여 아래쪽으로 밀리는 정도 조절
-            Spacer(minLength: 570)
-            
-            // "시작하기" 버튼
-            // 누르면 SignUpView로 이동
-            NavigationLink(destination: SignUpView()) {
-                Text("시작하기")
-                    .font(.system(size: 20))  // text font size 설정
-                    .foregroundColor(.white)  // text color 설정 (white)
-                    .frame(width: 350, height: 50)  // 버튼 크기 설정
-                    .background(Color.blue)  // 버튼 배경색 설정 (하늘색)
-                    .cornerRadius(5.0)  // 버튼 모서리를 둥글게
-            }
-            .padding(.bottom, 5)  // 버튼 아래 패딩 추가
-            
-            // "계정이 이미 있어요" 버튼
-            // 누르면 일단 콘솔에 메시지를 출력
-            // 이 버튼을 눌렀을 때 로그인 화면으로 이동하도록 설정
-            Button(action: {
-                print("이미 계정이 있음")
-            }) {
-                Text("계정이 이미 있어요")
-                    .font(.system(size: 20))
-                    .foregroundColor(.black)
-                    .frame(width: 350, height: 50)
-                    .background(Color.white)
-                    .overlay(RoundedRectangle(cornerRadius: 5)  // 테두리 둥글게
-                                .stroke(Color.black, lineWidth: 1))  // 테두리 색상 검은색으로
-            }
-          Spacer() // 아래쪽으로 밀어내는 Spacer
-        }
-    }
-}
-
-// SignUpView를 정의 부분
-// 일단 단순히 텍스트만 표시하도록 작성되었지만,
-// 실제로는 이 곳에 회원가입 화면 디자인
-struct SignUpView: View {
-    var body: some View {
-        Text("회원가입 화면")
-    }
-}
-*/
-
 import SwiftUI
 
 struct OnboardingView: View {
+    @State private var selectedTab = 0
+    @EnvironmentObject var ownerview: OwnerView
+
     var body: some View {
         NavigationView {
             VStack {
-                Spacer(minLength: 570)
-                
-                NavigationLink(destination: SignUpView()) {
-                    Text("시작하기")
-                        .font(.system(size: 20))
-                        .foregroundColor(.white)
-                        .frame(width: 350, height: 50)
-                        .background(Color.blue)
-                        .cornerRadius(5.0)
+                Spacer(minLength: 0)
+
+                TabView(selection: $selectedTab) {
+                    ForEach(0..<3) { index in
+                        Image("feature\(index + 1)")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 300)  // 이미지 높이 조절
+                            .tag(index)
+                    }
                 }
-                .padding(.bottom, 5)
-                
-                NavigationLink(destination: LogInView()) {
-                    Text("계정이 이미 있어요")
-                        .font(.system(size: 20))
-                        .foregroundColor(.black)
-                        .frame(width: 350, height: 50)
-                        .background(Color.white)
-                        .overlay(RoundedRectangle(cornerRadius: 5)
-                                    .stroke(Color.black, lineWidth: 1))
+                .tabViewStyle(PageTabViewStyle())
+
+                // Custom page indicator
+                HStack {
+                    ForEach(0..<3) { index in
+                        Circle()
+                            .fill(selectedTab == index ? Color(red: 247/255, green: 202/255, blue: 246/255) : Color(red: 229/255, green: 229/255, blue: 229/255))
+                            .frame(width: 7, height: 7)  // 원의 크기 조절
+                            .padding(.horizontal, 1)
+                    }
                 }
+                .padding(.top, -150)
+
+                Spacer(minLength: 0) // Adjust this value to move the buttons
+                
+                Text("시작하기")
+                    .font(.system(size: 18))
+                    .foregroundColor(.white)
+                    .frame(width: 350, height: 50)
+                    .background(Color(red: 247/255, green: 202/255, blue: 246/255))
+                    .cornerRadius(5.0)
+                    .onTapGesture {
+                        ownerview.owner = .signinview
+                    }
+                
+                
+                
                 Spacer()
             }
         }
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
     }
 }
 
--- CampusConnect/PreView/SplashView.swift
+++ CampusConnect/PreView/SplashView.swift
@@ -9,39 +9,53 @@ import SwiftUI
 
 struct SplashView: View {
     @State private var isActive = false
-    @State private var remainingSeconds = 5  // 5초 카운트다운
+    @State private var remainingSeconds = 2  // 몇 초 할지
+    @EnvironmentObject var ownerview : OwnerView
 
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()  // 매 초마다 트리거 되는 타이머
 
     var body: some View {
-        VStack {
-            if remainingSeconds > 0 {  // 1보다 클 때만 카운트다운 텍스트를 표시
-                Text("\(remainingSeconds)")
-                    .padding()
-            }
-            
-            Spacer()  // Text 아래에 Spacer 추가하여 Text를 상단으로 이동
+        ZStack {  // ZStack을 사용하여 배경색과 이미지를 동시에 설정
+            Color.black  // 배경색을 검정색으로 설정
+                .ignoresSafeArea()  // safe area를 무시하여 전체 화면을 채움
 
-            if isActive {
-                OnboardingView()
-            } else {
-                Text("Splash 화면")
-                    .onAppear {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-                            withAnimation {
-                                self.isActive = true
+            // SplashName라는 이미지를 추가. 이 부분을 실제 이미지 이름으로 바꾸세요.
+            Image("SplashName")
+                .resizable()  // 이미지 크기 조절을 가능하게 함
+                .aspectRatio(contentMode: .fit)  // 이미지를 원본 비율로 맞춤
+                .frame(width: 200, height: 200)  // 이미지의 크기를 설정
+            
+            VStack {
+                Spacer()  // Text 아래에 Spacer 추가하여 Text를 상단으로 이동
+
+                if isActive {
+                    //OnboardingView()
+                } else {
+                    EmptyView()
+                        .onAppear {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                                withAnimation {
+                                    //self.isActive = true
+                                }
                             }
                         }
-                    }
-                    .onReceive(timer) { _ in  // 타이머로부터의 입력을 받음
-                        if self.remainingSeconds > 0 {
-                            self.remainingSeconds -= 1
+                        .onReceive(timer) { _ in  // 타이머로부터의 입력을 받음
+                            if self.remainingSeconds > 0 {
+                                self.remainingSeconds -= 1
+                            }
+                            if self.remainingSeconds == 0 {  // 0이 되면 isActive를 true로 설정하여 OnboardingView로 전환
+                                //self.isActive = true
+                                ownerview.owner = .onboardingview
+                            }
                         }
-                        if self.remainingSeconds == 0 {  // 0이 되면 isActive를 true로 설정하여 OnboardingView로 전환
-                            self.isActive = true
-                        }
-                    }
+                }
             }
         }
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
     }
 }
